e839a0dc8aa202812b20a71d924b162a8953393c
Himanshu <himanshu.kumar.singh@gmail.com>
2024-12-14 12:21:37 +0530

    model change

--- src/model.py
+++ src/model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
 
-dropout_value = 0.02
+dropout_value = 0.015
 
 class Net(nn.Module):
     def __init__(self):
@@ -11,16 +11,17 @@ class Net(nn.Module):
         # Input Block with dilated convolution
         self.convblock1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=2, dilation=2, bias=False),
-            nn.LeakyReLU(0.15),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(10),
             nn.Dropout(dropout_value)
         )
         self.pool0 = nn.MaxPool2d(kernel_size=2, stride=1, padding=1)
+        self.avgpool0 = nn.AvgPool2d(kernel_size=3, stride=1, padding=1)
 
         # CONVOLUTION BLOCK 1
         self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=22, kernel_size=(3, 3), padding=1, bias=False),
-            nn.LeakyReLU(0.15),
+            nn.Conv2d(in_channels=10, out_channels=22, kernel_size=(3, 3), padding=2, bias=False),
+            nn.LeakyReLU(0.2),
             nn.BatchNorm2d(22),
             nn.Dropout(dropout_value)
         )
@@ -29,43 +30,43 @@ class Net(nn.Module):
         self.convblock3 = nn.Sequential(
             nn.Conv2d(in_channels=22, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
             nn.BatchNorm2d(10),
-            nn.LeakyReLU(0.15)
+            nn.GELU()
         )
         self.pool1 = nn.MaxPool2d(2, 2)
         self.avgpool1 = nn.AvgPool2d(kernel_size=2, stride=1, padding=1)
 
         # CONVOLUTION BLOCK 2 with grouped convolution
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=10, out_channels=22, kernel_size=(3, 3), padding=1, groups=2, bias=False),
-            nn.LeakyReLU(0.15),            
+            nn.Conv2d(in_channels=10, out_channels=22, kernel_size=(3, 3), padding=2, groups=2, bias=False),
+            nn.GELU(),            
             nn.BatchNorm2d(22),
             nn.Dropout(dropout_value)
         )
 
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.avgpool2 = nn.AvgPool2d(kernel_size=2, stride=1, padding=1)
+        self.avgpool2 = nn.AvgPool2d(kernel_size=3, stride=1, padding=1)
 
         # Depthwise Separable Convolution
         self.convblock5 = nn.Sequential(
-            nn.Conv2d(in_channels=22, out_channels=22, kernel_size=(3, 3), padding=1, groups=22, bias=False),
-            nn.Conv2d(in_channels=22, out_channels=16, kernel_size=(1, 1), bias=False),
-            nn.LeakyReLU(0.15),            
-            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=22, out_channels=22, kernel_size=(3, 3), padding=2, groups=22, bias=False),
+            nn.BatchNorm2d(22),
+            nn.LeakyReLU(0.2),            
             nn.Dropout(dropout_value)
         )
 
         self.pool3 = nn.MaxPool2d(2, 2)
+        self.avgpool3 = nn.AvgPool2d(kernel_size=2, stride=1, padding=1)
 
         self.convblock6 = nn.Sequential(
-            nn.Conv2d(in_channels=16, out_channels=14, kernel_size=(3, 3), padding=1, bias=False),
-            nn.LeakyReLU(0.15),            
+            nn.Conv2d(in_channels=22, out_channels=14, kernel_size=(3, 3), padding=2, bias=False),
+            nn.GELU(),            
             nn.BatchNorm2d(14),
             nn.Dropout(dropout_value)
         )
 
         self.convblock7 = nn.Sequential(
             nn.Conv2d(in_channels=14, out_channels=12, kernel_size=(3, 3), padding=1, bias=False),
-            nn.LeakyReLU(0.15),            
+            nn.LeakyReLU(0.2),            
             nn.BatchNorm2d(12),
             nn.Dropout(dropout_value)
         )
@@ -82,15 +83,21 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.convblock1(x)
         x = self.pool0(x)
+        x = self.avgpool0(x)
+        
         x = self.convblock2(x)
         x = self.convblock3(x)
         x = self.pool1(x)
         x = self.avgpool1(x)
+        
         x = self.convblock4(x)
         x = self.pool2(x)
         x = self.avgpool2(x)
+        
         x = self.convblock5(x)
         x = self.pool3(x)
+        x = self.avgpool3(x)
+        
         x = self.convblock6(x)
         x = self.convblock7(x)
         x = self.gap(x)        
